b1e0c32b99b2ef8b7b65d3bbed642ee349927232
ara-rz <rz_@live.jp>
2026-08-09 16:24:44 +0900

    words.jsを別ファイルに。TutCode.jsを探し出してきて、デフォルトを日本語入力に変更。

--- jsLetters.js
+++ jsLetters.js
@@ -3,7 +3,7 @@ const scoreElement = document.getElementById('score');
 const wordElement = document.getElementById('word');
 let score = 0;
 let activeWords = []; // 現在画面にある単語群を管理
-const words = ["abc","def","ghi","jkl"];//'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//const words = ["abc","def","ghi","jkl"];
 let inputWord = ""
 // 1. ドロップする「単語」を一つ生成する関数
 function dropWord() {
@@ -13,8 +13,8 @@ function dropWord() {
     letter.className = 'letter';
     // ランダムな位置から落とす
     letter.style.left = Math.random() * (window.innerWidth - 50) + 'px';
-    // ランダムな速度（3s〜8s）
-    const duration = Math.random() * 5 + 3;
+    // ランダムな速度（5秒〜15秒で落ちる様に…）
+    const duration = Math.random() * 10 + 5;
     letter.style.animationDuration = duration + 's';
     container.appendChild(letter);
     activeWords.push({
@@ -31,11 +31,14 @@ function dropWord() {
 }
 
 // 2. 文字を定期的に生成 (1000==1秒ごと)
-setInterval(dropWord, 2000);
-
+setInterval(dropWord, 3000);
+let tut = Tut()
 // 3. キーボード入力を受け取る
 document.addEventListener('keydown', (e) => {
-    inputWord += e.key;
+    let kChar = tut.feed(e.keyCode);/* 複数キー入力によって確定した かな文字(一文字)を返す */
+    //kChar = e.key    
+    if (kChar == "") {return;}/* 未確定の場合すぐリターン */
+    inputWord += kChar; //inputWord += e.key;
     // 入力された単語が、現在落下中の単語群のどれかと一致するか確認
     // (ここでは簡単のため、最初に見つかった単語のみ消す)
     const index = activeWords.findIndex(l => l.word === inputWord);
